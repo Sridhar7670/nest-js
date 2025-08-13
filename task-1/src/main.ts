17464fc2a7f48eb42f7c0836fb37441fa4a7af35
Sridhar AlpineCode <sridhar@thealpinecode.com>
2025-08-13 13:03:51 +0530

    render -3 headers

--- task-1/src/main.ts
+++ task-1/src/main.ts
@@ -4,20 +4,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Enable global validation pipe to automatically validate incoming data against DTOs
 
+  // Enable CORS with specific headers
   app.enableCors({
-    origin: ['https://frontendapp-5ou7.onrender.com/','http://localhost:3000'], // The origin of your deployed frontend
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: ['https://frontendapp-5ou7.onrender.com', 'http://localhost:3001'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization', // Allow Authorization header
     credentials: true,
   });
   
   app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Strips properties that do not have any decorators
-    forbidNonWhitelisted: true,  // throws error on unknown props
-    transform: true, // Automatically transform payloads to be objects typed according to their DTO classes
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    transform: true,
   }));
+  
   await app.listen(process.env.PORT || 3000);
-  console.log(`Application is running on: http://localhost:3000`);
 }
 bootstrap();
